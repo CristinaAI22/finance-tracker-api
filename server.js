a647f33a4715f106ff8c12992155608e58140eef
CristinaAI22 <cristina.ionica22@gmail.com>
2025-05-21 17:32:10 +0300

    adding routes, refactor jwt token

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(`API is running`);
---
+++ utils/generateToken.js
@@ -0,0 +1,8 @@
+import jwt from "jsonwebtoken";
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: "7d" });
+};
+
+export default generateToken;
