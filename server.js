22745ba76be35684881835df30255802afb6c2e1
CristinaAI22 <cristina.ionica22@gmail.com>
2025-05-22 15:29:29 +0300

    error and authenticate middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,10 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send(`API is running`);
 });
+
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
