0ef422d8971e4d0cefcb86b68d6fe41477f45c6b
CristinaAI22 <cristina.ionica22@gmail.com>
2025-05-22 14:55:30 +0300

    create transaction model

---
+++ models/Transaction.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    description: String,
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
--- models/User.js
+++ models/User.js
@@ -5,6 +5,5 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
-const User = mongoose.model("User", userSchema);
 
-export { User, userSchema };
+export const User = mongoose.model("User", userSchema);
